a5b2d2200fdbf9575c599779c9f6d124896ac4a1
MuYanustc <jun233@mail.ustc.edu.cn>
2024-11-15 23:18:29 +0800

    on pc at dorm

---
+++ Code/3239.minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=3239 lang=cpp
+ * @lcpr version=30204
+ *
+ * [3239] 最少翻转次数使二进制矩阵回文 I
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,0,0],[0,0,0],[0,0,1]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,1],[0,1],[0,0]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1],[0]]\n
+// @lcpr case=end
+
+ */
+
